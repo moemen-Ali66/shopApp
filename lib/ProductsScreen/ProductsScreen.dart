2a1703eda7b4389e7beae19b68cdb74e96403421
moemen-Ali66 <91580289+moemen-Ali66@users.noreply.github.com>
2022-12-06 04:26:11 +0200

    ff

--- lib/ProductsScreen/ProductsScreen.dart
+++ lib/ProductsScreen/ProductsScreen.dart
@@ -1,9 +1,55 @@
+import 'dart:html';
+
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
+import 'package:shop_app/cubit/Cubit.dart';
+import 'package:shop_app/models/HomeModel.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 class ProductScreen extends StatelessWidget {
   const ProductScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body:ConditionalBuilder(
+          condition:  ShopCubit.get(context).homeModel !=null ,
+          builder: (context)=>productItem(ShopCubit.get(context).homeModel!),
+          fallback: (context)=>Center(child: CircularProgressIndicator(),)),
+    );
+
   }
 }
+
+Widget productItem(HomeModel model)=>Column(
+  children: [
+    CarouselSlider(
+        items:model.data?.banners.map((e)=>  Image(
+          image: NetworkImage('${e.image}'),
+          width: double.infinity,
+          fit: BoxFit.cover,
+        )).toList() ,
+        options: CarouselOptions(
+          height: 250.0,
+          initialPage: 0,
+          viewportFraction: 1.0,
+          reverse: false,
+          enableInfiniteScroll: true,
+          autoPlay: true,
+          autoPlayInterval: Duration(seconds: 3),
+          autoPlayAnimationDuration: Duration(seconds: 1),
+          autoPlayCurve: Curves.fastOutSlowIn,
+          scrollDirection: Axis.horizontal,
+        )),
+    SizedBox(height: 20),
+    Expanded(
+        child: GridView.count(
+          crossAxisCount:2,
+          children:List.generate(
+              model.data!.products.length,
+                  (index) => builderproduct())
+          ,
+    ))
+  ],
+);
+
+Widget builderproduct()=>Column();
